5ff0864c124e8ad9ab70970bad5fdeee2cf43bcd
Harshit kyal <harshitkyal@gmail.com>
2017-12-05 12:22:31 +0530

    improve index and formatting of code

--- angular/route.js
+++ angular/route.js
@@ -1,29 +1,31 @@
+
+
 // routing path 
-angular.module('eplApp').config(function($routeProvider){
-  $routeProvider
-    .when('/:type/:matchDetails/:date/:tCode/:tCode2',{
-      templateUrl: 'pages/matchDetails.html',
-      controller: 'matchDetails',
-      controllerAs: 'firstCtrl'
-    })
-    
-    .when('/:type/:teamDetails/:tCode',{
-      templateUrl: 'pages/teamDetails.html',
-      controller: 'teamSpecific',
-      controllerAs: 'teamSpe'
-    })
-     .when('/:type/:leagueDetails',{
-      templateUrl: 'pages/leagueDetails.html',
-      controller: 'engPreLea',
-      controllerAs: 'EPL'   
-     })
-    .when('/epl/:type/league/Summary',{
-      templateUrl: 'pages/leagueSummary.html',
-      controller: 'leagueSummary15_16',
-      controllerAs: 'teamCtrl'   
-     })
-  
-    .otherwise({redirectTo: '/index.html'});
+angular.module('eplApp').config(function($routeProvider) {
+    $routeProvider
+        .when('/:type/:matchDetails/:date/:tCode/:tCode2', {
+            templateUrl: 'pages/matchDetails.html',
+            controller: 'matchDetails',
+            controllerAs: 'firstCtrl'
+        })
+
+        .when('/:type/:teamDetails/:tCode', {
+            templateUrl: 'pages/teamDetails.html',
+            controller: 'teamSpecific',
+            controllerAs: 'teamSpe'
+        })
+        .when('/:type/:leagueDetails', {
+            templateUrl: 'pages/leagueDetails.html',
+            controller: 'engPreLea',
+            controllerAs: 'EPL'
+        })
+        .when('/epl/:type/league/Summary', {
+            templateUrl: 'pages/leagueSummary.html',
+            controller: 'leagueSummary15_16',
+            controllerAs: 'teamCtrl'
+        })
+
+        .otherwise({
+            redirectTo: '/index.html'
+        });
 });
-
-
